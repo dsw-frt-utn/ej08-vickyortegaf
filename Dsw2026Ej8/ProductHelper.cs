a2b15f811ac134a8bc4a4a5a01514b90ef452604
Victoria Ortega <vickyortefer@gmail.com>
2026-05-02 19:57:45 -0300

    Finalizacion problema 1

---
+++ Dsw2026Ej8/ProductHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class ProductHelper
+    {
+        public string ObtenerEtiquetaProducto(long code, string description, decimal price)
+        {
+            string precioFormateado = FormatearPrecioComoMoneda(price);
+
+        }
+          
+    }
+}
