935f750b0dbd2e0f83fe4cd2347a958509a425bc
Victoria Ortega <vickyortefer@gmail.com>
2026-05-03 16:35:29 -0300

    Resolucion Problema 6

---
+++ Dsw2026Ej8/Problema6.cs
@@ -0,0 +1,10 @@
+﻿namespace Dsw2026Ej8
+{
+    public class Problema6
+    {
+        public string NormalizarCodigoProducto(string code)
+        {
+            return code.ProductoCodigo();
+        }
+    }
+}
