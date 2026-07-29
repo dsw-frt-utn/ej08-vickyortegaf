97034353ed9381d84cb64762019ffa201162295e
vickyortegaf <victoriaortegafernandez@alu.frt.utn.edu.ar>
2026-07-28 22:36:26 -0300

    Problema 5 completado

--- Dsw2026Ej8/Problema5.cs
+++ Dsw2026Ej8/Problema5.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Dsw2026Ej8
+﻿namespace Dsw2026Ej8
 {
-    internal class Problema5
+    public class Problema5
     {
+        public decimal ObtenerImporteFinal(Sale sale)
+        {
+            return sale.CalculateTotal();
+        }
     }
 }
